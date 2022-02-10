650542ba1c41c9bd7f6465707f6cbee1163f290c
gracekabaghe <gracema2002@gmail.com>
2022-02-10 16:38:35 +0200

    Write tests for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -36,5 +36,34 @@ describe Solver do
         end
     end
 
+    describe "#fizzbuzz" do
+        it "returns 'fizz' when passed a multiple of 3" do
+            expect(Solver).to receive(:fizzbuzz).with(3).and_return("fizz")
+            expect(Solver).to receive(:fizzbuzz).with(6).and_return("fizz")
+            expect(Solver).to receive(:fizzbuzz).with(99).and_return("fizz")
+        end
+
+        it "returns 'buzz' when passed a multiple of 5" do
+            expect(Solver).to receive(:fizzbuzz).with(5).and_return("buzz")
+            expect(Solver).to receive(:fizzbuzz).with(10).and_return("buzz")
+            expect(Solver).to receive(:fizzbuzz).with(95).and_return("buzz")
+        end
+
+        it "returns 'fizzbuzz' when passed a multiple of 3 and 5" do
+            expect(Solver).to receive(:fizzbuzz).with(15).and_return("fizzbuzz")
+            expect(Solver).to receive(:fizzbuzz).with(30).and_return("fizzbuzz")
+            expect(Solver).to receive(:fizzbuzz).with(105).and_return("fizzbuzz")
+        end
+        it "returns the number when passed a number that is not a multiple of 3 or 5" do
+            expect(Solver).to receive(:fizzbuzz).with(1).and_return(1.to_s)
+            expect(Solver).to receive(:fizzbuzz).with(2).and_return(2.to_s)
+            expect(Solver).to receive(:fizzbuzz).with(4).and_return(4.to_s)
+            expect(Solver).to receive(:fizzbuzz).with(7).and_return(7.to_s)
+            expect(Solver).to receive(:fizzbuzz).with(8).and_return(8.to_s)
+            expect(Solver).to receive(:fizzbuzz).with(11).and_return(64.to_s)
+            expect(Solver).to receive(:fizzbuzz).with(13).and_return(67.to_s)
+            expect(Solver).to receive(:fizzbuzz).with(16).and_return(83.to_s)
+        end
+    end
   end
 end
