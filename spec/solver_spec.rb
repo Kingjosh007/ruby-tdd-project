32bb00d7531d5b564c0e2880ca19a69a8f43e10d
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-02-10 15:24:32 +0100

    Write test suites for factorial and reverse methods

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,10 +1,40 @@
+require_relative '../solver'
 
 describe Solver do
+
   context 'When testing the Book class' do
-    it 'A newly created book has a title of type String' do
-      bk = Book.new('The Hobbit', 'J.R.R. Tolkien', [])
-      expect(bk.title).not_to be_nil
-      expect(bk.title).to be_a(String)
+
+    describe "#factorial" do
+      it "returns 1 when 0 is passed" do
+
+        expect(Solver).to receive(:factorial).with(0).and_return(1)
+      end
+
+      it "returns 2 when 2 is passed" do
+        expect(Solver).to receive(:factorial).with(2).and_return(2)
+      end
+
+      it "returns 120 when 5 is passed" do
+        expect(Solver).to receive(:factorial).with(5).and_return(120)
+      end
+
+      it "raises an exception when a negative number is passed" do
+        expect { Solver.factorial(-1) }.to raise_error(ArgumentError)
+        expect { Solver.factorial(-10) }.to raise_error(ArgumentError)
+      end
     end
+
+    describe "#reverse" do
+        it "returns the reverse of the string passed" do
+            expect(Solver).to receive(:reverse).with("abc").and_return("cba")
+            expect(Solver).to receive(:reverse).with("okay").and_return("yako")
+        end
+
+        it "returns the same string when a palindrom is passed" do
+            expect(Solver).to receive(:reverse).with("kayaksaladalaskayak").and_return("kayaksaladalaskayak")
+            expect(Solver).to receive(:reverse).with("radar").and_return("radar")
+        end
+    end
+
   end
 end
